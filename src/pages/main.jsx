766b11a8a598cdb3c2cdb3467b3bdccdd52600a2
Baekshook <ecgo0106@naver.com>
2024-03-08 15:47:27 +0900

    first mainPage setting

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./animate.css";
+import StartButtons from "../components/StartButtons";
 
 export default function Main() {
+  const flavorCheck = "입맛\n첵췤";
   return (
-    <div className="bg-pink-300 min-h-screen flex flex-col justify-center items-center">
-      <div className="text-6xl font-bold">이상형 월드컵</div>
-      <Link to="/worldcup">
-        <button className=" bg-pink-400 text-white px-4 py-2 rounded-lg mt-8">
-          시작
-        </button>
-      </Link>
+    <div className="bg-purple-300 min-h-screen flex flex-col justify-center items-center">
+      <div className="relative w-[360px] h-[800px] flex flex-col justify-center items-center">
+        <img
+          className="absolute"
+          src={`${process.env.PUBLIC_URL}/images/bg.png`}
+          alt="animals"
+        />
+        <div className="card mb-24">
+          <div className="bg">
+            <div className="font-bold text-7xl justify-center items-center flex ml-3 leading-tight tracking-[15px] whitespace-pre-wrap">
+              {flavorCheck}
+            </div>
+          </div>
+          <div className="blob"></div>
+        </div>
+        <StartButtons></StartButtons>
+        <Link to="/worldcup"></Link>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blobBounce: {
+          "0%, 25%, 50%, 75%, 100%": {
+            transform: "translate(-100%, -100%) translate3d(0, 0, 0)",
+          },
+        },
+      },
+      animation: {
+        blobBounce: "5s infinite ease",
+      },
+    },
   },
   plugins: [],
 };
